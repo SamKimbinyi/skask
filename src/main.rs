827917b87ab5a0d162714fee096ad4265613d99f
Sam Kimbinyi <sam@kimbinyi.com>
2022-07-13 21:02:07 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use clap::{Parser,Subcommand};
+
+#[derive(Parser)]
+#[clap(author,version,about,long_about = None)]
+struct Cli{
+    #[clap(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands{
+    /// Open a new or existing Skask datastore
+    Open{
+        #[clap(value_parser)]
+        directory: Option<String>,
+
+    },
+}
+
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command{
+            Commands::Open {directory} =>{
+                println!("Opening {:?} ",directory)
+            }
+    }
+
+}
